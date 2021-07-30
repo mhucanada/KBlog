c79c1d8deb114dbd22effb408a7619ad0c1e5226
mhucanada <mhucanada1@gmail.com>
2021-07-29 22:00:35 -0400

    uibuild

--- backend/app.js
+++ backend/app.js
@@ -29,7 +29,7 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 
 app.use('/api/tasks', taskRouter)
-app.use('/api/categories', categoryRouter)
+app.use('/api/categorys', categoryRouter)
 app.use('/api/users', usersRouter)
 
 app.use(middleware.unknownEndpoint)
